c05ae59dce05d18918cbd50587c8533619b9d443
Maxi S <maxi0062009@hotmail.com>
2024-05-14 18:38:00 -0300

    Cargo navBar minimas modificaciones en Style.css

---
+++ Js/NavBar.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbarContainer = document.getElementById("navbar-container");
+
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            navbarContainer.innerHTML = data;
+
+            // Traigo el botón toggler y el navbar
+            var navbarToggler = document.querySelector('.navbar-toggler');
+            var navbar = document.getElementById('navbar');
+
+            // Cargo de Bt donde esta el collapse de la hamb
+            var collapse = new bootstrap.Collapse(navbar.querySelector('.navbar-collapse'));
+
+            // Hover en el toggler
+            function openTogglerOnHover() {
+                var target = navbarToggler.getAttribute('data-bs-target');
+                var collapseElement = document.querySelector(target);
+                if (!collapseElement.classList.contains('show')) {
+                    collapse.show();
+                }
+            }
+
+            navbarToggler.addEventListener('mouseenter', openTogglerOnHover);
+
+            // Cierro el menú desplegable al hacer clic en otra parte de la página
+            document.addEventListener('click', function (event) {
+                var isClickInsideNavbar = navbar.contains(event.target);
+                if (!isClickInsideNavbar && collapse._isShown) {
+                    collapse.hide();
+                }
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar la barra de navegación:", error);
+        });
+});
+
